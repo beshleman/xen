45fe6f69a4fa0d817b28c3c1ff79c868714c7851
Bobby Eshleman <bobby.eshleman@starlab.io>
2019-10-01 20:18:29 -0500

    fixup! riscv: stub out guest_atomics.h

--- xen/include/asm-riscv/guest_atomics.h
+++ xen/include/asm-riscv/guest_atomics.h
@@ -4,26 +4,52 @@
 /*
  * TODO: implement guest atomics
  */
-#define guest_set_bit(d, nr, p)         \
-    do { \
-        (void) d;       \
-        (void) nr;      \
-        (void) p;       \
-    } while(0)
 
-#define guest_clear_bit(d, nr, p)   \
-    do { \
-        (void) d;       \
-        (void) nr;      \
-        (void) p;       \
-    } while(0)
 
-#define guest_change_bit(d, nr, p)  (0)
-#define guest_test_bit(d, nr, p)    (0)
-#define guest_test_and_set_bit(d, nr, p) (0)
-#define guest_test_and_clear_bit(d, nr, p) (0)
-#define guest_test_and_change_bit(d, nr, p) (0)
-#define guest_cmpxchg(d, ptr, o, n) (0)
+/* TODO */
+#define guest_testop(name)                                                  \
+static inline int guest_##name(struct domain *d, int nr, volatile void *p)  \
+{                                                                           \
+    (void) d;       \
+    (void) nr;      \
+    (void) p;       \
+                                                                            \
+    return 0;                                                               \
+}
+
+
+/* TODO */
+#define guest_bitop(name)                                                   \
+static inline void guest_##name(struct domain *d, int nr, volatile void *p) \
+{                                                                           \
+    (void) d;                                                               \
+    (void) nr;                                                              \
+    (void) p;                                                               \
+}
+
+guest_bitop(set_bit)
+guest_bitop(clear_bit)
+guest_bitop(change_bit)
+
+#undef guest_bitop
+
+guest_testop(test_and_set_bit)
+guest_testop(test_and_clear_bit)
+guest_testop(test_and_change_bit)
+
+#undef guest_testop
+
+#define guest_test_bit(d, nr, p) ((void)(d), test_bit(nr, p))
+
+static inline int guest_cmpxchg(void *d, void *ptr, uint32_t o, uint32_t n)
+{                
+    (void) d;       
+    (void) ptr;     
+    (void) o;       
+    (void) n;       
+
+    return 0;
+}
 
 #endif /* _RISCV_GUEST_ATOMICS_H */
 /*
