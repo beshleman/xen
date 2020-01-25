7e2d134f9d637cf3e578ed151ee7b6a1a24e339d
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-01-25 11:02:38 -0600

    fixup! riscv: header files

--- xen/include/asm-riscv/iommu.h
+++ xen/include/asm-riscv/iommu.h
@@ -31,9 +31,6 @@ struct arch_iommu
     void *priv;
 };
 
-/* Always share P2M Table between the CPU and the IOMMU */
-#define iommu_use_hap_pt(d) (has_iommu_pt(d))
-
 const struct iommu_ops *iommu_get_ops(void);
 void iommu_set_ops(const struct iommu_ops *ops);
 
